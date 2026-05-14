b1bb35b2bb9f45aef8fac06578fc3ed03d618dd4
mihail <vbdfann@gmail.com>
2026-05-14 20:13:38 +0300

    настроил конфиги

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST=os.getenv("DB_HOST", "localhost")
+DB_PORT=os.getenv("DB_PORT", "5432")
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_NAME=os.getenv("DB_NAME")
+
+JWT_SECRET=os.getenv("JWT_SECRET")
+JWT_ALGORITHM=os.getenv("JWT_ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES= int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+REFRESH_TOKEN_EXPIRE_DAYS=int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
