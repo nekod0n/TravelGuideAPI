261515595f797aa2130a66591b12c6863b7d9e02
mihail <vbdfann@gmail.com>
2026-05-14 20:14:11 +0300

    подключился к БД с помощью конфигов

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import  create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import DB_NAME, DB_PORT, DB_USER, DB_PASSWORD, DB_HOST
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
